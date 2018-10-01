397954bd31cca183d6b837002c7fd7c24be79409
Vikos <afanasievavv@gmail.com>
2018-10-01 19:11:41 +0300

    second commit

--- src/index.js
+++ src/index.js
@@ -2,14 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './containers/App';
 import Add from './components/Add';
-import { Router, Route, IndexRoute, browserHistory } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+// import createBrowserHistory from 'history/createBrowserHistory'
+
+// const history = createBrowserHistory()
 
 ReactDOM.render(
-	<Router history={browserHistory}>
-		<Route path='/' component={App}>
-			<IndexRoute component={App} />
-			<Route path='add' component={Add} />
-		</Route>
+	<Router>
+		<Route exact path="/" component={App} />
+		<Route exact path="/add" component={Add} />
 	</Router>, 
 	document.getElementById('root')
 );
